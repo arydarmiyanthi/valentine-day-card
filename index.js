9df56e9951bf918f365c747b09780c26b3a8af34
arydarmiyanthi <arydarmiyanthi@gmail.com>
2025-02-14 20:41:27 +0800

    menambahkan musik

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-$(document).ready(function() {
-    var envelope = $("#envelope");
-    var btn_open = $("#open");
-    var btn_reset = $("#reset");
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
 
-    envelope.click(function() {
-        open();
-    });
-    btn_open.click(function() {
-        open();
-    });
-    btn_reset.click(function() {
-        close();
-    });
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
 
-    function open() {
-        envelope.addClass("open")
-            .removeClass("close");
-    }
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
 
-    function close() {
-        envelope.addClass("close")
-            .removeClass("open");
-    }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
 
-})
+  function open() {
+    envelope.addClass("open").removeClass("close");
+    var audio = document.getElementById("valentine-audio");
+    audio.play(); // Memutar musik saat amplop dibuka
+  }
 
+  function close() {
+    envelope.addClass("close").removeClass("open");
+    var audio = document.getElementById("valentine-audio");
+    audio.pause(); // Menghentikan musik saat amplop ditutup
+    audio.currentTime = 0; // Reset waktu ke awal
+  }
+});
